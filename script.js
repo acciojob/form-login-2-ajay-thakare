7f8093e01665eb9f5683b2a2a0391226ed462d6d
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-07-03 20:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-//your JS code here. If required.
 const input1 = document.getElementById("fName");
     const input2 = document.getElementById("lName");
     const input3 = document.getElementById("phone");
     const input4 = document.getElementById("email");
 
-    function submit() {
+    const form = document.querySelector("#myForm");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
       alert(
         `First Name: ${input1.value} Last Name: ${input2.value} Phone Number: ${input3.value} Email ID: ${input4.value}`,
       );
-    }
+    });
